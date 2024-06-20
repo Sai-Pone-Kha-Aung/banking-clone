30463c052740607020769d06a78ba2a693bc3635
Sai Pone Kha Aung <saiponekhaaaung@gmail.com>
2024-06-20 10:45:49 +0700

    Home Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import HeaderBox from "@/components/atoms/HeaderBox"
+import TotalBalanceBox from "@/components/atoms/TotalBalanceBox"
 
 const HomePage = () => {
+  const loggedIn = {firstName: "John"}
   return (
-    <div>HomePage</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome to MWE Bank"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access & manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1000}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
